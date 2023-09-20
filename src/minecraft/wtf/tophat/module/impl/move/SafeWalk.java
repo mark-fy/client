4242205878d340e520d7b6a5288a9a3d9e6cb3ae
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-09-20 18:36:12 +0300

    safewalk module

---
+++ src/minecraft/wtf/tophat/module/impl/move/SafeWalk.java
@@ -0,0 +1,21 @@
+package wtf.tophat.module.impl.move;
+
+import io.github.nevalackin.radbus.Listen;
+import wtf.tophat.events.impl.SafeWalkEvent;
+import wtf.tophat.module.base.Module;
+import wtf.tophat.module.base.ModuleInfo;
+import wtf.tophat.utilities.Methods;
+
+@ModuleInfo(name = "SafeWalk", desc = "doesn't let you go off edges", category = Module.Category.MOVE)
+public class SafeWalk extends Module {
+
+    @Listen
+    public void onSafe(SafeWalkEvent event) {
+        if(Methods.mc.player == null || Methods.mc.world == null)
+            return;
+
+
+        event.setSafe(mc.player.onGround);
+    }
+
+}
