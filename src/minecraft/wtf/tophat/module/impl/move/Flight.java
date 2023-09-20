0b6afb6ac88dd8a9f639a7fab6cd6a635f1307e2
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-09-20 18:28:48 +0300

    Verus & Collision Flight

--- src/minecraft/wtf/tophat/module/impl/move/Flight.java
+++ src/minecraft/wtf/tophat/module/impl/move/Flight.java
@@ -1,13 +1,17 @@
 package wtf.tophat.module.impl.move;
 
 import io.github.nevalackin.radbus.Listen;
+import net.minecraft.util.AxisAlignedBB;
 import org.lwjgl.input.Keyboard;
 import wtf.tophat.Client;
+import wtf.tophat.events.Event;
+import wtf.tophat.events.impl.CollisionBoxesEvent;
 import wtf.tophat.events.impl.MotionEvent;
 import wtf.tophat.module.base.Module;
 import wtf.tophat.module.base.ModuleInfo;
 import wtf.tophat.settings.impl.StringSetting;
 import wtf.tophat.settings.impl.NumberSetting;
+import wtf.tophat.utilities.Methods;
 import wtf.tophat.utilities.movement.MoveUtil;
 
 @ModuleInfo(name = "Flight",desc = "fly like a bird", category = Module.Category.MOVE)
@@ -18,14 +22,35 @@ public class Flight extends Module {
 
     public Flight() {
         Client.settingManager.add(
-                mode = new StringSetting(this, "Mode", "Vanilla", "Vanilla"),
+                mode = new StringSetting(this, "Mode", "Vanilla", "Vanilla", "Collision", "Verus"),
                 speed = new NumberSetting(this, "Speed", 0, 4, 1, 2)
         );
     }
 
+    // Verus
+    private boolean up;
+
     @Listen
     public void onMotion(MotionEvent event) {
         switch ((mode.get())){
+            case "Verus":
+                if(event.state == Event.State.PRE) {
+                    if (!mc.settings.keyBindJump.isKeyDown()) {
+                        if (mc.player.onGround) {
+                            mc.player.motionY = 0.42f;
+                            up = true;
+                        } else if (up) {
+                            if (!mc.player.isCollidedHorizontally) {
+                                mc.player.motionY = -0.0784000015258789;
+                            }
+                            up = false;
+                        }
+                    } else if (mc.player.ticksExisted % 3 == 0) {
+                        mc.player.motionY = 0.42f;
+                    }
+                    MoveUtil.setSpeed(mc.settings.keyBindJump.isKeyDown() ? 0 : 0.33);
+                }
+                break;
             case "Vanilla":
                 mc.player.motionY = 0;
 
@@ -40,5 +65,33 @@ public class Flight extends Module {
                 MoveUtil.setSpeed(speed.get().floatValue());
                 break;
         }
+    }
+
+    @Listen
+    public void onCollision(CollisionBoxesEvent event) {
+        if(Methods.mc.player == null || Methods.mc.world == null)
+            return;
+
+        switch (mode.get()) {
+            case "Verus":
+                event.setBoundingBox(new AxisAlignedBB(-5, -1, -5, 5, 1, 5).offset(event.getBlockPos().getX(), event.getBlockPos().getY(), event.getBlockPos().getZ()));
+                break;
+            case "Collision":
+                if(!mc.settings.keyBindSneak.pressed)
+                    event.setBoundingBox(new AxisAlignedBB(-2, -1, -2, 2, 1, 2).offset(event.getBlockPos().getX(), event.getBlockPos().getY(), event.getBlockPos().getZ()));
+                break;
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        up = false;
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        up = false;
+        super.onDisable();
     }
 }
