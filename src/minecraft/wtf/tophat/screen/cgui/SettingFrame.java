9178a05b98aad8aef50afb356a367e0feea7cf4e
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-09-29 18:43:12 +0300

    fix: colors for settings

--- src/minecraft/wtf/tophat/screen/cgui/SettingFrame.java
+++ src/minecraft/wtf/tophat/screen/cgui/SettingFrame.java
@@ -78,9 +78,9 @@ public class SettingFrame extends GuiScreen {
             if(setting instanceof StringSetting) {
                 DrawingUtil.rectangle(x, y + 18, width, height, true, new Color(33,33,33));
                 fr.drawStringChoose(shadow,setting.getName().toLowerCase(Locale.ROOT) + ": ", (int) x + 5, (int) y + 25, Color.WHITE);
-                DrawingUtil.rectangle(x + 187 - fr.getStringWidth(((StringSetting) setting).getValue()), y + 22, fr.getStringWidth(((StringSetting) setting).getValue()) + 3, 11, true, new Color(20,20,20));
-                DrawingUtil.rectangle(x + 187 - fr.getStringWidth(((StringSetting) setting).getValue()), y + 22, fr.getStringWidth(((StringSetting) setting).getValue()) + 3, 11, false, color);
-                fr.drawStringChoose(shadow, ((StringSetting) setting).getValue().toLowerCase(Locale.ROOT), (int) (x + 187 - fr.getStringWidth(((StringSetting) setting).getValue()) + 1), (int) y + 22 + 2, Color.WHITE);
+                DrawingUtil.rectangle(x + 192 - fr.getStringWidth(((StringSetting) setting).getValue().toLowerCase(Locale.ROOT)), y + 22, fr.getStringWidth(((StringSetting) setting).getValue().toLowerCase(Locale.ROOT)) + 3, 11, true, new Color(20,20,20));
+                DrawingUtil.rectangle(x + 192 - fr.getStringWidth(((StringSetting) setting).getValue().toLowerCase(Locale.ROOT)), y + 22, fr.getStringWidth(((StringSetting) setting).getValue().toLowerCase(Locale.ROOT)) + 3, 11, false, new Color(ColorUtil.fadeBetween(DEFAULT_COLOR, colorLighter.getRGB(), counter * 150L)));
+                fr.drawStringChoose(shadow, ((StringSetting) setting).getValue().toLowerCase(Locale.ROOT), (int) (x + 192 - fr.getStringWidth(((StringSetting) setting).getValue().toLowerCase(Locale.ROOT)) + 1), (int) y + 22 + 2, Color.WHITE);
                 y += 20;
             }
 
@@ -88,7 +88,6 @@ public class SettingFrame extends GuiScreen {
                 DrawingUtil.rectangle(x, y + 18, width, height, true, new Color(33,33,33));
                 fr.drawStringChoose(shadow,setting.getName().toLowerCase(Locale.ROOT) + ": ", (int) x + 4, (int) y + 25, Color.WHITE);
                 DrawingUtil.rectangle(x + 174, y + 22, 21, 11, true, new Color(20,20,20));
-                // new Color(ColorUtil.fadeBetween(WHITE_COLOR, LIGHT_GRAY_COLOR, counter * 150L))
                 DrawingUtil.rectangle(x + 174, y + 22, 21, 11, false, ((BooleanSetting) setting).getValue() ? new Color(ColorUtil.fadeBetween(DEFAULT_COLOR, colorLighter.getRGB(), counter * 150L)) : new Color(ColorUtil.fadeBetween(WHITE_COLOR, LIGHT_GRAY_COLOR, counter * 150L)));
                 DrawingUtil.rectangle(((BooleanSetting) setting).getValue() ? x + 185 : x + 175, y + 23, 9, 9, true, ((BooleanSetting) setting).getValue() ? colorLighter : new Color(255, 255, 255));
                 y += 20;
@@ -104,14 +103,14 @@ public class SettingFrame extends GuiScreen {
 
                 DrawingUtil.rectangle(x, y + 18, width, height + 18, true, new Color(33, 33, 33));
 
-                String formattedValue = String.format(Locale.ROOT, setting.getName() + ": %." + decimalPoints + "f", currentValue);
+                String formattedValue = String.format(Locale.ROOT, setting.getName().toLowerCase(Locale.ROOT) + ": %." + decimalPoints + "f", currentValue);
 
                 fr.drawStringChoose(shadow, formattedValue.toLowerCase(Locale.ROOT), (int) x + 4, (int) y + 25, Color.WHITE);
 
                 DrawingUtil.rectangle(x + 5, y + 40, 185, 11, true, new Color(0, 0, 0));
                 DrawingUtil.rectangle(x + 5, y + 40, randoValue, 11, true, new Color(60, 60, 60));
-                DrawingUtil.rectangle(sliderPosition, y + 40, 6, 11, true, new Color(125, 125, 125));
-                DrawingUtil.rectangle(x + 5, y + 40, 185, 11, false, color);
+                DrawingUtil.rectangle(sliderPosition, y + 40, 6, 11, true, new Color(ColorUtil.fadeBetween(WHITE_COLOR, LIGHT_GRAY_COLOR, counter * 150L)));
+                DrawingUtil.rectangle(x + 5, y + 40, 185, 11, false, new Color(ColorUtil.fadeBetween(DEFAULT_COLOR, colorLighter.getRGB(), counter * 150L)));
                 y += 38;
             }
             counter++;
@@ -127,7 +126,7 @@ public class SettingFrame extends GuiScreen {
 
         for (Setting setting : Client.settingManager.getSettingsByModule(parent)) {
             if (setting instanceof StringSetting) {
-                double boxX = x + 187 - fr.getStringWidth(((StringSetting) setting).getValue()), boxY = y + 22, boxWidth = fr.getStringWidth(((StringSetting) setting).getValue()) + 3, boxHeight = 11;
+                double boxX = x + 192 - fr.getStringWidth(((StringSetting) setting).getValue()), boxY = y + 22, boxWidth = fr.getStringWidth(((StringSetting) setting).getValue()) + 3, boxHeight = 11;
 
                 if (mouseX >= boxX && mouseX <= boxX + boxWidth && mouseY >= boxY && mouseY <= boxY + boxHeight) {
                     if (mouseButton == 0) {
