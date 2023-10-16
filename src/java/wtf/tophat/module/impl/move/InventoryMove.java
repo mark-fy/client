0041af396ea2345b5bb0df23e71a777cacfe4b76
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-10-16 18:51:39 +0300

    misc: made inventory move bypass

--- src/java/wtf/tophat/module/impl/move/InventoryMove.java
+++ src/java/wtf/tophat/module/impl/move/InventoryMove.java
@@ -3,14 +3,51 @@ package wtf.tophat.module.impl.move;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.network.play.client.C0DPacketCloseWindow;
+import net.minecraft.network.play.client.C16PacketClientStatus;
+import net.minecraft.network.play.server.S2DPacketOpenWindow;
+import net.minecraft.network.play.server.S2EPacketCloseWindow;
 import org.lwjgl.input.Keyboard;
+import wtf.tophat.Client;
 import wtf.tophat.events.base.Event;
 import wtf.tophat.events.impl.MotionEvent;
+import wtf.tophat.events.impl.PacketEvent;
 import wtf.tophat.module.base.Module;
 import wtf.tophat.module.base.ModuleInfo;
+import wtf.tophat.settings.impl.BooleanSetting;
 
 @ModuleInfo(name = "Inventory Move",desc = "move in guis", category = Module.Category.MOVE)
 public class InventoryMove extends Module {
+
+    private final BooleanSetting noOpenPacket, xCarry;
+
+    public InventoryMove() {
+        Client.settingManager.add(
+                noOpenPacket = new BooleanSetting(this, "No Open Packet", false),
+                xCarry = new BooleanSetting(this, "Extra Storage", false)
+        );
+    }
+
+
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if (getPlayer() == null || getWorld() == null)
+            return;
+
+        if(event.getType() == PacketEvent.Type.OUTGOING) {
+            if(noOpenPacket.get() && event.getPacket() instanceof S2DPacketOpenWindow || event.getPacket() instanceof S2EPacketCloseWindow) {
+                event.setCancelled(true);
+            }
+
+            if(xCarry.get() && event.getPacket() instanceof C0DPacketCloseWindow) {
+                event.setCancelled(true);
+            }
+
+            if(event.getPacket() instanceof C16PacketClientStatus && ((C16PacketClientStatus) event.getPacket()).getStatus() == C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT) {
+                event.setCancelled(true);
+            }
+        }
+    }
 
     @Listen
     public void onMotion(MotionEvent event) {
@@ -27,8 +64,7 @@ public class InventoryMove extends Module {
                     break block3;
                 }
                 for (KeyBinding bind : moveKeys) {
-                    if (Keyboard.isKeyDown(bind.getKeyCode()))
-                        continue;
+                    if (Keyboard.isKeyDown(bind.getKeyCode()))  continue;
                     KeyBinding.setKeyBindState(bind.getKeyCode(), false);
                 }
             }
