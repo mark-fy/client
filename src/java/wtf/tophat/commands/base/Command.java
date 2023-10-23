68debe27e1e5b43bab18437a45838305924f023a
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-10-23 15:30:56 +0300

    misc: shortened code

--- src/java/wtf/tophat/commands/base/Command.java
+++ src/java/wtf/tophat/commands/base/Command.java
@@ -3,7 +3,6 @@ package wtf.tophat.commands.base;
 import wtf.tophat.utilities.Methods;
 
 public class Command implements Methods {
-
     public String name = this.getClass().getAnnotation(CommandInfo.class).name();
     public String description = this.getClass().getAnnotation(CommandInfo.class).description();
     public String command = this.getClass().getAnnotation(CommandInfo.class).command();
@@ -11,8 +10,6 @@ public class Command implements Methods {
     public void onCommand(String[] args, String command) {}
 
     public String getName() { return name; }
-
     public String getDescription() { return description; }
-
     public String getCommand() { return command; }
 }
