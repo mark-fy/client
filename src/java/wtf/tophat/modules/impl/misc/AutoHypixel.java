23ef1234c16b4e27d22e8c0eb16592ade0228846
v4n1ty <splitz.off@proton.me>
2023-10-29 00:59:23 +0200

    Added AutoHypixel (I don't try it)

---
+++ src/java/wtf/tophat/modules/impl/misc/AutoHypixel.java
@@ -0,0 +1,57 @@
+package wtf.tophat.modules.impl.misc;
+
+import io.github.nevalackin.radbus.Listen;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.network.play.server.S3EPacketTeams;
+import net.minecraft.network.play.server.S45PacketTitle;
+import net.minecraft.util.StringUtils;
+import wtf.tophat.events.impl.PacketEvent;
+import wtf.tophat.modules.base.Module;
+import wtf.tophat.modules.base.ModuleInfo;
+import wtf.tophat.utilities.time.Stopwatch;
+
+@ModuleInfo(name = "AutoHypixel", desc = "join automatically the game when you finish / lost a game", category = Module.Category.EXPLOIT)
+public class AutoHypixel extends Module {
+
+    public String knownMode, knownType;
+    public Stopwatch delay = new Stopwatch();
+
+    @Listen
+    public void onPacket(PacketEvent e) {
+        if ((e.getPacket() instanceof S3EPacketTeams)) {
+            String message = StringUtils.stripControlCodes(((S3EPacketTeams) e.getPacket()).getPrefix());
+            if (message.equals("Mode: Normal")) {
+                knownMode = "normal";
+            }
+            if (message.equals("Mode: Insane")) {
+                knownMode = "insane";
+            }
+            if (message.equals("Mode: Mega")) {
+                knownType = "mega";
+                knownMode = "normal";
+            }
+            if (message.equals("Teams left")) {
+                knownType = "teams";
+            }
+        }
+        if (e.getPacket() instanceof S45PacketTitle) {
+            if (((S45PacketTitle) e.getPacket()).getMessage() == null)
+                return;
+
+            String message = ((S45PacketTitle) e.getPacket()).getMessage().getUnformattedText();
+            if (message.equals("YOU DIED!") || message.equals("GAME END") || message.equals("VICTORY!") || message.equals("You are now a spectator!")) {
+                if (knownType != null && knownMode != null) {
+                    if (delay.hasTimeElapsed(2000, true)) {
+                        mc.player.sendChatMessage("/play " + knownType + "_" + knownMode);
+                    }
+                }
+            }
+        }
+        if (e.getPacket() instanceof S02PacketChat) {
+            String message = ((S02PacketChat) e.getPacket()).getChatComponent().getUnformattedText();
+            if (message.equals("Teaming is not allowed on Solo mode!")) {
+                knownType = "solo";
+            }
+        }
+    }
+}
