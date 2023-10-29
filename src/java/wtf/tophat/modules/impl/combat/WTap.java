82a0dd4040fe5a4b52ed8ef6133fb7a83bc53860
v4n1ty <splitz.off@proton.me>
2023-10-29 22:42:52 +0100

    Added WTap

---
+++ src/java/wtf/tophat/modules/impl/combat/WTap.java
@@ -0,0 +1,28 @@
+package wtf.tophat.modules.impl.combat;
+
+import io.github.nevalackin.radbus.Listen;
+import net.minecraft.network.play.client.C02PacketUseEntity;
+import net.minecraft.network.play.client.C0BPacketEntityAction;
+import wtf.tophat.events.impl.PacketEvent;
+import wtf.tophat.modules.base.Module;
+import wtf.tophat.modules.base.ModuleInfo;
+
+@ModuleInfo(name = "WTap", desc = "press W and undpress for combos", category = Module.Category.COMBAT)
+public class WTap extends Module {
+
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if (mc.world != null && mc.player != null) {
+            if(event.getPacket() instanceof C02PacketUseEntity) {
+                final C02PacketUseEntity packet = (C02PacketUseEntity) event.getPacket();
+                if ((packet.getAction() == C02PacketUseEntity.Action.ATTACK) && (packet.getEntityFromWorld(mc.world) != mc.player) && (mc.player.getFoodStats().getFoodLevel() > 6)) {
+                    boolean sprint = mc.player.isSprinting();
+                    mc.player.setSprinting(false);
+                    mc.player.sendQueue.send(new C0BPacketEntityAction(mc.player, C0BPacketEntityAction.Action.STOP_SPRINTING));
+                    mc.player.sendQueue.send(new C0BPacketEntityAction(mc.player, C0BPacketEntityAction.Action.START_SPRINTING));
+                    mc.player.setSprinting(sprint);
+                }
+            }
+        }
+    }
+}
