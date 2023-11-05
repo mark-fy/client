cbabe952b6638815418ee66e8bdf1c8aaa587703
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-05 12:11:06 +0200

    add: new autohypixel

--- src/java/wtf/tophat/modules/impl/misc/AutoHypixel.java
+++ src/java/wtf/tophat/modules/impl/misc/AutoHypixel.java
@@ -1,56 +1,130 @@
 package wtf.tophat.modules.impl.misc;
 
 import io.github.nevalackin.radbus.Listen;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.play.client.C01PacketChatMessage;
 import net.minecraft.network.play.server.S02PacketChat;
-import net.minecraft.network.play.server.S3EPacketTeams;
-import net.minecraft.network.play.server.S45PacketTitle;
-import net.minecraft.util.StringUtils;
+import net.minecraft.util.EnumChatFormatting;
+import wtf.tophat.TopHat;
 import wtf.tophat.events.impl.PacketEvent;
+import wtf.tophat.events.impl.UpdateEvent;
 import wtf.tophat.modules.base.Module;
 import wtf.tophat.modules.base.ModuleInfo;
+import wtf.tophat.settings.impl.BooleanSetting;
+import wtf.tophat.settings.impl.NumberSetting;
+import wtf.tophat.settings.impl.StringSetting;
 import wtf.tophat.utilities.time.TimeUtil;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @ModuleInfo(name = "Auto Hypixel", desc = "automatically navigate inside hypixel", category = Module.Category.MISC)
 public class AutoHypixel extends Module {
 
-    public String knownMode, knownType;
-    private TimeUtil timer = new TimeUtil();
+    private final BooleanSetting autoReconnect, antiBan, autoPlay, autoBounty, skyWars;
+    private final StringSetting skyWarsMode;
+    private final NumberSetting skyWarsDelay, minBounty;
+
+    // Sky Wars
+    private final TimeUtil timer = new TimeUtil();
+
+    // Auto Bounty
+    private final List<Entity> entities = new CopyOnWriteArrayList<>();
+
+    public AutoHypixel() {
+        TopHat.settingManager.add(
+                autoReconnect = new BooleanSetting(this, "Auto Reconnect", true),
+                antiBan = new BooleanSetting(this, "Anti Ban", true),
+                autoPlay = new BooleanSetting(this, "Auto Play", true),
+                skyWars = new BooleanSetting(this, "SkyWars", true).setHidden(() -> !autoPlay.get()),
+                skyWarsMode = new StringSetting(this, "SkyWars Mode", "Solo Normal", "Solo Normal", "Solo Insane").setHidden(() -> !autoPlay.get() || !skyWars.get()),
+                skyWarsDelay = new NumberSetting(this, "SkyWars Delay", 0, 5000, 1000, 0).setHidden(() -> !autoPlay.get() || !skyWars.get()),
+                autoBounty = new BooleanSetting(this, "Auto Bounty", false),
+                minBounty = new NumberSetting(this, "Minimum Bounty", 50, 5000, 500, 0).setHidden(() -> !autoBounty.get())
+        );
+    }
 
     @Listen
-    public void onPacket(PacketEvent e) {
-        if ((e.getPacket() instanceof S3EPacketTeams)) {
-            String message = StringUtils.stripControlCodes(((S3EPacketTeams) e.getPacket()).getPrefix());
-            if (message.equals("Mode: Normal")) {
-                knownMode = "normal";
-            }
-            if (message.equals("Mode: Insane")) {
-                knownMode = "insane";
-            }
-            if (message.equals("Mode: Mega")) {
-                knownType = "mega";
-                knownMode = "normal";
-            }
-            if (message.equals("Teams left")) {
-                knownType = "teams";
-            }
-        }
-        if (e.getPacket() instanceof S45PacketTitle) {
-            if (((S45PacketTitle) e.getPacket()).getMessage() == null)
-                return;
+    public void onUpdate(UpdateEvent event) {
+        if(autoBounty.get()) {
+            entities.removeIf(player -> !mc.world.playerEntities.contains(player));
 
-            String message = ((S45PacketTitle) e.getPacket()).getMessage().getUnformattedText();
-            if (message.equals("YOU DIED!") || message.equals("GAME END") || message.equals("VICTORY!") || message.equals("You are now a spectator!")) {
-                if (knownType != null && knownMode != null) {
-                    if (timer.elapsed(2000, true)) {
-                        mc.player.sendChatMessage("/play " + knownType + "_" + knownMode);
+            double maxDistanceSq = 200 * 200;
+
+            for (Entity entity : mc.world.loadedEntityList) {
+                if (entity != mc.player && entity instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) entity;
+
+                    double distanceSq = mc.player.getDistanceSqToEntity(player);
+
+                    if (distanceSq <= maxDistanceSq) {
+                        String display = player.getDisplayName().getUnformattedText();
+                        String name = player.getName();
+
+                        if (display.contains("§6§l")) {
+                            String[] split = display.split(" ");
+                            if (split.length > 2) {
+                                int bounty = Integer.parseInt(
+                                        split[split.length - 1]
+                                                .replace("§6§1l", "")
+                                                .replace("g", "")
+                                );
+                                if (bounty >= minBounty.get().intValue()) {
+                                    if (!entities.contains(player)) {
+                                        entities.add(player);
+                                        sendChat(name + " has " + bounty + "g on him!", true);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        if (e.getPacket() instanceof S02PacketChat) {
-            String message = ((S02PacketChat) e.getPacket()).getChatComponent().getUnformattedText();
-            if (message.equals("Teaming is not allowed on Solo mode!")) {
-                knownType = "solo";
+    }
+
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if(getPlayer() == null || getWorld() == null)
+            return;
+
+        if(autoReconnect.get()) {
+            if(event.getPacket() instanceof S02PacketChat) {
+                String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(((S02PacketChat)event.getPacket()).getChatComponent().getUnformattedText()), text = unformatted.replace(" ", "");
+                if(unformatted.contains("Flying or related")) {
+                    sendPacketUnlogged(new C01PacketChatMessage("/back"));
+                }
+            }
+        }
+
+        if(autoPlay.get()) {
+            if(event.getPacket() instanceof S02PacketChat && skyWars.get()) {
+                String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(((S02PacketChat)event.getPacket()).getChatComponent().getUnformattedText()), text = unformatted.replace(" ", "");
+
+                String victory = "You won! Want to play again? Click here!";
+                String death = "You died! Want to play again? Click here!";
+
+                if(unformatted.contains(victory) || unformatted.contains(death) && timer.elapsed(skyWarsDelay.get().longValue())) {
+                    sendPacketUnlogged(new C01PacketChatMessage(skyWarsMode.is("Solo Normal") ? "/play solo_normal" : "/play solo_insane"));
+                }
+            }
+        }
+
+        if(antiBan.get()) {
+            if(event.getPacket() instanceof S02PacketChat) {
+                String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(((S02PacketChat)event.getPacket()).getChatComponent().getUnformattedText()), text = unformatted.replace(" ", "");
+                if (unformatted.contains("A player has been removed from your game.")) {
+                    new Thread(() -> {
+                        try {
+                            Thread.sleep(500);
+                        }catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        mc.player.sendChatMessage("/hub");
+                        sendChat("Left the game due to someone being banned!", true);
+                    }).start();
+                }
             }
         }
     }
