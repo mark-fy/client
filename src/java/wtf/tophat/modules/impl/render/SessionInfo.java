94203d299b5aa1b09a02afc21931c265578c0cdb
v4n1ty <splitz.off@proton.me>
2023-11-07 21:31:07 +0100

    Better Session Info

--- src/java/wtf/tophat/modules/impl/render/SessionInfo.java
+++ src/java/wtf/tophat/modules/impl/render/SessionInfo.java
@@ -3,10 +3,15 @@ package wtf.tophat.modules.impl.render;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S02PacketChat;
 import wtf.tophat.events.impl.OnDeathEvent;
+import wtf.tophat.events.impl.PacketEvent;
 import wtf.tophat.events.impl.Render2DEvent;
+import wtf.tophat.events.impl.UpdateEvent;
 import wtf.tophat.modules.base.Module;
 import wtf.tophat.modules.base.ModuleInfo;
+import wtf.tophat.utilities.network.ServerUtil;
 import wtf.tophat.utilities.render.DrawingUtil;
 
 import java.awt.*;
@@ -14,7 +19,8 @@ import java.awt.*;
 @ModuleInfo(name = "Session Info", desc = "displays info about the current session", category = Module.Category.RENDER)
 public class SessionInfo extends Module {
 
-    private int deaths = 0;
+    public int deaths = 0;
+    public int kills = 0;
 
     @Override
     public void onDisable() {
@@ -28,14 +34,53 @@ public class SessionInfo extends Module {
     }
 
     @Listen
+    public void onPacket(PacketEvent event) {
+        Packet<?> e = event.getPacket();
+        if (e instanceof S02PacketChat) {
+            S02PacketChat s02PacketChat = (S02PacketChat)e;
+            String cp21 = s02PacketChat.getChatComponent().getUnformattedText();
+            if (cp21.contains("was killed by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was slain by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was shot and killed by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was snowballed to death by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was killed with magic by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was killed with an explosion by " + mc.session.getUsername())) {
+                kills++;
+            }
+
+            if (cp21.contains("was killed with a potion by " + mc.session.getUsername())) {
+                kills++;
+            }
+        }
+    }
+
+    @Listen
     public void onRender(Render2DEvent event) {
+        int x = 62;
+        int y = 62;
         ScaledResolution sr = event.getScaledResolution();
         FontRenderer fr = mc.fontRenderer;
 
-        // WIP UI
-        DrawingUtil.rectangle(60, 60, 120, 40, true, new Color(45,45, 45));
+        DrawingUtil.rectangle(60, 60, 120, 52, true, new Color(45,45, 45));
         fr.drawStringWithShadow("Session Info", 62, 62, Color.BLUE);
-        fr.drawStringWithShadow("Deaths: " + deaths, 62, 72, Color.RED);
+        fr.drawString("Play Time: " + ServerUtil.getSessionLengthString(), x, y + 30, -1);
+        fr.drawString("Kills: " + kills, x, y + 10, -1);
+        fr.drawString("Deaths: " + deaths, x, y + 20, -1);
+        fr.drawString("KDR: " + Math.max(1, this.kills) / Math.max(1, this.deaths), x, y + 40, -1);
     }
-
 }
--- src/java/wtf/tophat/utilities/network/ServerUtil.java
+++ src/java/wtf/tophat/utilities/network/ServerUtil.java
@@ -1,20 +1,25 @@
 package wtf.tophat.utilities.network;
 
-import net.minecraft.client.multiplayer.ServerData;
+import java.util.HashMap;
+import java.util.Map;
 import wtf.tophat.utilities.Methods;
 
-public class ServerUtil implements Methods {
-    public static boolean onServer(final String server) {
-        final ServerData serverData = mc.getCurrentServerData();
-        return serverData != null && serverData.serverIP.toLowerCase().contains(server);
+public final class ServerUtil implements Methods {
+    private static final Map<String, Long> serverIpPingCache;
+    public static long timeJoined;
+    public static String serverIp;
+
+    public static String getSessionLengthString() {
+        serverIp = mc.getCurrentServerData() != null ? ServerUtil.mc.getCurrentServerData().serverIP : "Singleplayer";
+        long totalSeconds = (System.currentTimeMillis() - timeJoined) / 1000L;
+        long hours = totalSeconds / 3600L;
+        long minutes = totalSeconds % 3600L / 60L;
+        long seconds = totalSeconds % 60L;
+        return (hours > 0L ? hours + "h " : "") + minutes + "m " + seconds + "s";
     }
 
-    public static boolean onHypixel() {
-        final ServerData serverData = mc.getCurrentServerData();
-
-        if (serverData == null)
-            return false;
-
-        return serverData.serverIP.endsWith("hypixel.net") || serverData.serverIP.endsWith("hypixel.net:25565") || serverData.serverIP.equals("104.17.71.15") || serverData.serverIP.equals("104.17.71.15:25565");
+    static {
+        timeJoined = System.currentTimeMillis();
+        serverIpPingCache = new HashMap<String, Long>();
     }
 }
