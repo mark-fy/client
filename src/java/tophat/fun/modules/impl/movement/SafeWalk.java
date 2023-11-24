e014d7f8928462dbc0b57240f2b021422996b071
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-25 00:11:05 +0200

    add: safewalk

---
+++ src/java/tophat/fun/modules/impl/movement/SafeWalk.java
@@ -0,0 +1,18 @@
+package tophat.fun.modules.impl.movement;
+
+import io.github.nevalackin.radbus.Listen;
+import tophat.fun.events.impl.player.SafeWalkEvent;
+import tophat.fun.modules.Module;
+import tophat.fun.modules.ModuleInfo;
+
+@ModuleInfo(name = "SafeWalk", desc = "prevents you from falling off edges.", category = Module.Category.MOVEMENT)
+public class SafeWalk extends Module {
+
+    @Listen
+    public void onSafe(SafeWalkEvent event) {
+        if (mc.thePlayer == null || mc.theWorld == null)
+            return;
+
+        event.setSafe(mc.thePlayer.onGround);
+    }
+}
