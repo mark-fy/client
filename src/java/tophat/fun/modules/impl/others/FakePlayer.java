d771d21734d445147024b85bc0ead83af0a3c771
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-25 00:12:03 +0200

    fix: fakeplayer not allowing to disable

--- src/java/tophat/fun/modules/impl/others/FakePlayer.java
+++ src/java/tophat/fun/modules/impl/others/FakePlayer.java
@@ -14,8 +14,8 @@ public class FakePlayer extends Module {
 
     @Override
     public void onEnable() {
-        if(mc.thePlayer == null || !mc.thePlayer.isDead || mc.theWorld == null){
-            //setEnabled(false);
+        if(mc.theWorld == null || mc.thePlayer == null || mc.thePlayer.isDead ){
+            setEnabled(false);
         }
 
         EntityOtherPlayerMP clonedPlayer = new EntityOtherPlayerMP(Methods.mc.theWorld, new GameProfile(UUID.fromString("9b7f28c2-98ea-4d70-b2db-48e6c78a4a9d"), "TestSubject"));
@@ -36,6 +36,7 @@ public class FakePlayer extends Module {
         if (mc.theWorld != null) {
             mc.theWorld.removeEntityFromWorld(-4200);
         }
+
         super.onDisable();
     }
 
