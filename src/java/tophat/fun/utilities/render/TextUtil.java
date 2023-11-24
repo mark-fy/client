e248ae1264076914d48fc37d261c07a2aae21d2e
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-24 19:04:46 +0200

    misc: moved text utils

--- src/java/tophat/fun/utilities/render/TextUtil.java
+++ src/java/tophat/fun/utilities/render/TextUtil.java
@@ -3,6 +3,9 @@ package tophat.fun.utilities.render;
 import tophat.fun.modules.Module;
 import tophat.fun.utilities.Methods;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TextUtil implements Methods {
 
     public static String getCategoryLetter(Module.Category category) {
@@ -19,6 +22,30 @@ public class TextUtil implements Methods {
         } else {
             return "d";
         }
+    }
+
+    public static String createAsteriskString(String input) {
+        StringBuilder asterisks = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            asterisks.append('*');
+        }
+        return asterisks.toString();
+    }
+
+    public static boolean isPrintableChar(char c) {
+        return c >= 32 && c <= 126;
+    }
+
+    public static int isColorCode(String input) {
+        Pattern pattern = Pattern.compile("§[a-zA-Z]");
+        Matcher matcher = pattern.matcher(input);
+
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        return count;
     }
 
 }
