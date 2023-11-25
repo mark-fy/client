ce5b95a1aa7cde56372326d70286ab3b235c1813
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-25 11:14:13 +0200

    add: session info

---
+++ src/java/tophat/fun/modules/impl/design/SessionInfo.java
@@ -0,0 +1,65 @@
+package tophat.fun.modules.impl.design;
+
+import io.github.nevalackin.radbus.Listen;
+import net.minecraft.network.play.server.S02PacketChat;
+import tophat.fun.events.impl.network.PacketEvent;
+import tophat.fun.events.impl.player.DeathEvent;
+import tophat.fun.events.impl.render.Render2DEvent;
+import tophat.fun.modules.Module;
+import tophat.fun.modules.ModuleInfo;
+import tophat.fun.utilities.font.CFont;
+import tophat.fun.utilities.font.renderer.TTFFontRenderer;
+import tophat.fun.utilities.others.ServerUtil;
+import tophat.fun.utilities.render.shader.DrawHelper;
+
+import java.awt.*;
+
+@ModuleInfo(name = "SessionInfo", desc = "displays info about the current session.", category = Module.Category.DESIGN)
+public class SessionInfo extends Module {
+
+    public int deaths = 0;
+    public int kills = 0;
+
+    private final static TTFFontRenderer poppins = CFont.FONT_MANAGER.getFont("PoppinsMedium 18");
+    private final static TTFFontRenderer poppinsR = CFont.FONT_MANAGER.getFont("PoppinsRegular 18");
+
+    @Listen
+    public void onPacket(PacketEvent event) {
+        if (event.getPacket() instanceof S02PacketChat) {
+            String cp21 = ((S02PacketChat)event.getPacket()).getChatComponent().getUnformattedText();
+            String username = mc.session.getUsername();
+            if (cp21.matches(".*was (killed|slain|shot and killed|snowballed to death|killed with (magic|an explosion|a potion)) by " + username + ".*")) {
+                kills++;
+            }
+        }
+    }
+
+    @Listen
+    public void on2D(Render2DEvent event) {
+        float x = 62;
+        float y = 62;
+        float width = 130;
+        float height = 52;
+
+        DrawHelper.drawRoundedRect( x, y, width, height, 6, new Color(25,25,25));
+        DrawHelper.drawRoundedRectOutline( x - 1, y - 1, width + 2, height + 2, 6, 2, new Color(24, 175, 162));
+
+        poppins.drawString("Session Info", x + 2, y, -1);
+        poppinsR.drawString("Play Time: " + ServerUtil.getSessionLength(), x + 2, y + 30, -1);
+        poppinsR.drawString("Kills: " + kills, x + 2, y + 10, -1);
+        poppinsR.drawString("Deaths: " + deaths, x + 2, y + 20, -1);
+        poppinsR.drawString("KDR: " + Math.max(1, this.kills) / Math.max(1, this.deaths), x + 2, y + 40, -1);
+    }
+
+    @Override
+    public void onDisable() {
+        deaths = 0;
+        super.onDisable();
+    }
+
+    @Listen
+    public void onDeath(DeathEvent event) {
+        deaths++;
+    }
+
+}
---
+++ src/java/tophat/fun/utilities/others/ServerUtil.java
@@ -0,0 +1,23 @@
+package tophat.fun.utilities.others;
+
+import tophat.fun.utilities.Methods;
+
+public class ServerUtil implements Methods {
+
+    public static long timeJoined;
+    public static String serverIp;
+
+    public static String getSessionLength() {
+        serverIp = mc.getCurrentServerData() != null ? mc.getCurrentServerData().serverIP : "Singleplayer";
+        long totalSeconds = (System.currentTimeMillis() - timeJoined) / 1000L;
+        long hours = totalSeconds / 3600L;
+        long minutes = totalSeconds % 3600L / 60L;
+        long seconds = totalSeconds % 60L;
+        return (hours > 0L ? hours + "h " : "") + minutes + "m " + seconds + "s";
+    }
+
+    static {
+        timeJoined = System.currentTimeMillis();
+    }
+
+}
