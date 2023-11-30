fdd8fa751230e322507180c74aca6ab867c85a43
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-11-30 18:46:29 +0200

    misc: changed version

--- src/java/tophat/fun/Information.java
+++ src/java/tophat/fun/Information.java
@@ -1,7 +1,7 @@
 package tophat.fun;
 
 public interface Information {
-    String CNAME = "TopHat", CVERSION = "b112423";
+    String CNAME = "TopHat", CVERSION = "b113023";
     String[] CAUTHORS = new String[] {"MarkGG", "fyre", "sootysplash", "heritsy", "Dimeus", "DisPro.jar"};
     String CPREFIX = "§9" + CNAME + " §7:: ";
 }
