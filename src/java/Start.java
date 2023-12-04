546c6fa01d3dfad6ac5c0a9135d60f303fde7806
heristy <124876844+heristydev@users.noreply.github.com>
2023-12-04 20:01:44 +0100

    d

--- src/java/Start.java
+++ src/java/Start.java
@@ -1,13 +1,16 @@
 import java.io.File;
+<<<<<<< Updated upstream
 import java.lang.reflect.Field;
+=======
+>>>>>>> Stashed changes
 import java.util.Arrays;
 
 import net.minecraft.client.main.Main;
 
 public class Start
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        System.setProperty("org.lwjgl.librarypath", new File("../natives/").getAbsolutePath());
         Main.main(concat(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 
