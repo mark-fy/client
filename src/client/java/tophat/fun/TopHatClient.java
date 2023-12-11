07f48f453f36a16969b7050e7370e364b8de0d68
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-12-11 19:57:29 +0200

    add: gradle base

---
+++ src/client/java/tophat/fun/TopHatClient.java
@@ -0,0 +1,10 @@
+package tophat.fun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TopHatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
