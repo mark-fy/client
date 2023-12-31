8150ad65f97e3cf3378c2adcfc509615ee10f446
scooty splasher <125141643+sootysplash@users.noreply.github.com>
2023-12-31 13:31:18 +0400

    o7

--- src/java/tophat/fun/Information.java
+++ src/java/tophat/fun/Information.java
@@ -3,6 +3,6 @@ package tophat.fun;
 public interface Information {
     String CNAME = "TopHat", CVERSION = "b120923";
     String CLOADTEXT = "Loading TopHat...";
-    String[] CAUTHORS = new String[] {"MarkGG", "fyre", "sootysplash", "heritsy"};
+    String[] CAUTHORS = new String[] {"MarkGG", "fyre", "heritsy"};
     String CPREFIX = "§9" + CNAME + " §7:: ";
 }
